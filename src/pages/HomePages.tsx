c6de19810b4f2b9fcef3425e47537a00f9e6244c
Mohamed Abdo <mohamedabdoelatar@gmail.com>
2024-05-03 22:35:23 +0300

    render

--- src/pages/HomePages.tsx
+++ src/pages/HomePages.tsx
@@ -2,8 +2,8 @@ import React, { Component } from "react";
 
 interface IProps {}
 interface IState {
-counter:number,
-products:[],
+  counter: number;
+  products: [];
 }
 
 class HomePages extends Component<IProps, IState> {
@@ -13,15 +13,31 @@ class HomePages extends Component<IProps, IState> {
     this.state = {
       counter: 0,
       products: [],
-    };
+    }
+    // fire for the firat time
+    console.log("%c constructor", "background-color:green;");
+    
   }
   render() {
+    // fire for every change in componenta
+  
+    console.log("%c re-render", "background-color:red;");
     return (
       <div>
         <h1>HomePage</h1>
         <h1>count:{this.state.counter}</h1>
+        <button
+          onClick={() => this.setState({ counter: this.state.counter + 1 })}
+        ></button>
       </div>
     );
   }
 }
 export default HomePages;
+
+/*
+
+1-constructor
+2-render
+
+*/
